29d5d24995e7e540938bf5b0e995391fc55bdcfb
shivangi-pandey <70841027+shivangi-pandey@users.noreply.github.com>
2022-01-27 11:23:26 +0530

    git push

---
+++ DSA/Arrays/array1.java
@@ -0,0 +1,22 @@
+package DSA.Arrays;
+
+public class array1 {
+   
+
+        public static void main(String[] args){
+            pattern1(5);
+        }
+        static void pattern1(int n){
+            //loop for number of rows
+            for(int i=1;i<=n;i++){
+                //2nd loop for number of columns
+                    for(int j=1;j<=5;j++){
+                        System.out.print("*");
+                    }
+                    //for next line
+                    System.out.println();
+            }
+        }
+    }
+    
+
