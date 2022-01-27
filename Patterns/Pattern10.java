993c750966d5a8e71f1cc41f8cb88e56f4e1c5a6
shivangi-pandey <70841027+shivangi-pandey@users.noreply.github.com>
2022-01-27 10:56:22 +0530

    First Commit

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,38 @@
+package Patterns;
+
+/*
+         *
+        * *
+       * * *
+      * * * *
+     * * * * *
+*/
+
+public class Pattern10 {
+    public static void main(String[] args){
+        pattern10(5);
+    }
+    
+    static void pattern10(int n){
+
+        for(int i=0;i<n;i++){
+            
+                for(int j=n-i;j>=1;j--){
+                   
+                        System.out.print(" ");
+                    }
+                    for(int j=0;j<=i;j++){
+                    {
+                        System.out.print("* ");
+                    }
+                   
+                }
+                System.out.println();
+                
+        }
+    
+    }
+
+    
+
+}
